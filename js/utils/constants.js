69165787161906019c130747220feee37254bbb5
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-29 19:38:19 +0900

    feat : 하드코딩 된 값 상수화

--- js/utils/constants.js
+++ js/utils/constants.js
@@ -4,6 +4,12 @@ const KEYBOARD = {
 
 const RESULT_VIEW_MESSAGES = {
   NO_RESULT: '검색어에 대한 정보가 없습니다.',
+  NO_KEYWORD: '추천 검색어가 없습니다.',
 };
 
-export { KEYBOARD, RESULT_VIEW_MESSAGES };
+const TAB_NAME = {
+  KEYWORD: '추천 검색어',
+  HISTORY: '최근 검색어',
+};
+
+export { KEYBOARD, RESULT_VIEW_MESSAGES, TAB_NAME };
